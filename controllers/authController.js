abb8ac5197eea8c4e4a49470fb87e6be4e10429d
JorgeCarrascoF <jorgecarfacpsico@gmail.com>
2025-08-02 15:10:24 +0200

    fixed case typo

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,7 @@
 // ============================================
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/user');
 
 const JWT_SECRET = process.env.JWT_SECRET || 'clave_secreta';
 
