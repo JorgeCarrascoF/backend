58361d06bf42c351d77af9c8cff61b727edc31b0
kenneth chan <david.kachi18@gmail.com>
2025-08-12 18:35:31 -0400

    feat(user) superadmin role added

--- routes/users.js
+++ routes/users.js
@@ -9,18 +9,19 @@ const {
     deleteUser,      // Importar nueva función
     getUsersByFilter
 } = require('../controllers/userController');
-const auth = require('../middleware/auth');
+const { authMiddleware, requireSuperAdmin, requireAdminOrSuper } = require('../middleware/auth');
+const { userPermissions } = require('../middleware/permissions');
 
 const router = express.Router();
 
 // Rutas GET
-router.get('/', auth, getUsersByFilter);
-router.get('/:id', auth, getUserById);
+router.get('/', authMiddleware, userPermissions.canRead, getUsersByFilter);
+router.get('/:id', authMiddleware, userPermissions.canRead, getUserById);
 
 // Nueva ruta PATCH para actualizar
-router.patch('/:id', auth, updateUser);
+router.patch('/:id', authMiddleware, userPermissions.canUpdate, updateUser);
 
-// Nueva ruta DELETE para eliminar
-router.delete('/:id', auth, deleteUser);
+// Nueva ruta DELETE para eliminar - solo superadmin
+router.delete('/:id', authMiddleware, requireSuperAdmin, deleteUser);
 
 module.exports = router;
