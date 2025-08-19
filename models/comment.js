a0998d2ed9ec45d35e9bfa1d18e91c1f1a951d8b
nicolas acuña <nicolassimonacuna@gmail.com>
2025-08-18 22:31:29 -0400

    Comentar logs

--- models/comment.js
+++ models/comment.js
@@ -1,11 +1,36 @@
 const mongoose = require('mongoose');
 
 const commentSchema = new mongoose.Schema({
-    comment: String,
-    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-    logId: {type: mongoose.Schema.Types.ObjectId, ref: 'Log'}    
+  text: String,
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  logId: { type: mongoose.Schema.Types.ObjectId, ref: 'Log' },
+  create_at: {
+    type: Date,
+    default: Date.now
+  }
+});
 
+commentSchema.set('toJSON', {
+  virtuals: true,
+  transform: (doc, ret) => {
+    if (ret.userId) {
+      ret.user = {
+        id: ret.userId._id.toString(),
+        email: ret.userId.email,
+        fullName: ret.userId.fullName
+      };
+      delete ret.userId;
+    }
+
+    if (ret.logId) {
+      ret.log = ret.logId;
+      delete ret.logId;
+    }
+
+    delete ret.__v;
+    return ret;
+  }
 });
 
 const Comment = mongoose.model('Comment', commentSchema);
-module.exports=Comment;
+module.exports = Comment;
