29e3ef7d40de76566f5679484c2788afa596eab8
JorgeCarrascoF <jorgecarfacpsico@gmail.com>
2025-08-25 09:03:21 +0200

    fix(cors): intento 2 de arreglar el cors

--- app.js
+++ app.js
@@ -33,6 +33,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -47,7 +49,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //   credentials: true,
 // }));
 
-app.use(cors());
+
 
 // Rutas principales
 app.use('/', indexRouter);
