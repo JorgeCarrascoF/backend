78c7621eb06ef7c95d2de0b7655c31de75ef50b6
Miguel <mickychog@gmail.com>
2025-08-26 13:49:23 -0400

    correo electronico

--- config/email.js
+++ config/email.js
@@ -16,18 +16,24 @@ const transporter = nodemailer.createTransport({
 
 const sendEmail = async (to, subject, html) => {
   try {
-    await transporter.sendMail({
-      from: `" - Buggle - " <${process.env.GMAIL_USER}>`,
+    console.log(`Intentando enviar correo a ${to}...`);
+    const info = await transporter.sendMail({
+      from: `"Buggle" <${process.env.GMAIL_USER}>`,
       to,
       subject,
       html,
     });
-    console.log(`Correo enviado correctamente a: ${to}`);
+    console.log(`Correo enviado a ${to}. Message ID: ${info.messageId}`);
     return true;
   } catch (error) {
-    console.error(`Error al enviar el correo a ${to}:`, error.message);
+    console.error(`Error al enviar correo a ${to}:`, {
+      message: error.message,
+      code: error.code,
+      response: error.response,
+    });
     return false;
   }
 };
+
 
 module.exports = { sendEmail };
